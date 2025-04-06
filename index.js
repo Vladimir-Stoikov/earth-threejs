369213e3e4c0bd39b4512f937f408f1909ef5859
Vladimir Stoikov <vspsmoke@gmail.com>
2025-04-06 20:26:28 +0300

    add simple sphere and light source

--- index.js
+++ index.js
@@ -13,4 +13,23 @@ const far = 10;
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 camera.position.z = 2;
 
-const scence = new THREE.Scene();
+const scene = new THREE.Scene();
+
+const geo = new THREE.IcosahedronGeometry(1.0, 3);
+const mat = new THREE.MeshStandardMaterial({
+  color: 0xffffff,
+  flatShading: true,
+});
+const mesh = new THREE.Mesh(geo, mat);
+scene.add(mesh);
+
+const hemiLight = new THREE.HemisphereLight(0xffffff, 0x000000);
+scene.add(hemiLight);
+
+function animate(t = 0) {
+  requestAnimationFrame(animate);
+  mesh.scale.setScalar(Math.cos(t * 0.001) + 1.0);
+  renderer.render(scene, camera);
+}
+
+animate();
